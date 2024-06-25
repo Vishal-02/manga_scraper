3dc2a8e9ca02e94daff8fa34d94b6c4c8baf4dab
Vishal Singh <vishal.aksingh@gmail.com>
2024-06-25 13:46:03 +0400

    implemented the needed changes, it works now. i'll see if i can get it to look better. would be nice to have a scrollbar and all that too.

--- explore/gui_test.py
+++ explore/gui_test.py
@@ -9,11 +9,16 @@ root.geometry('900x500')
 
 # configuring the styel
 style = ttk.Style()
-style.theme_use("darkly")
+style.theme_use("solar")
 style.configure(
-    "Treeview",
+    "Treeview.Heading",
     font=("Times New Roman", 14),
-    rowheight=100,
+)
+
+style.configure(
+    "Custom.Treeview",
+    rowheight = 50,
+    font=("Times New Roman", 14),
 )
 
 # topmost label
@@ -30,26 +35,32 @@ label.pack(fill="x", side="top", anchor="nw", pady=10)
 ttk.Separator(root, orient="horizontal", bootstyle="light").pack(fill="x")
 
 # the tree that shows all the manga in my database
-columns = ('Title', 'Currently Read', 'Latest Released', 'Download')
+columns = ('Title', 'Current', 'Latest', 'Download')
 manga_tree = ttk.Treeview(
     root,
     bootstyle="success",
     columns=columns,
-    show="headings"
+    show="headings",
+    style="Custom.Treeview"
 )
 
 manga_tree.heading('Title', text='Title')
 manga_tree.column("Title", minwidth=0, width=400, stretch=NO)
-manga_tree.heading('Currently Read', text='Currently Read')
-manga_tree.column("Currently Read", minwidth=0, width=120, stretch=NO)
-manga_tree.heading('Latest Released', text='Latest Released')
-manga_tree.column("Latest Released", minwidth=0, width=120, stretch=NO)
+manga_tree.heading('Current', text='Current')
+manga_tree.column("Current", minwidth=0, width=120, stretch=NO)
+manga_tree.heading('Latest', text='Latest')
+manga_tree.column("Latest", minwidth=0, width=120, stretch=NO)
 manga_tree.heading('Download', text='Download')
 
+manga_tree.tag_configure('oddRow', background="#333333", foreground="#FFFFFF")
+manga_tree.tag_configure('evenRow', background="#444444", foreground="#FFFFFF")
+
 manga_values = all_db_data()
-for row in manga_values:
-    manga_tree.insert('', END, values=row)
+for i, row in enumerate(manga_values):
+    tag = "evenRow" if (i % 2 == 0) else "oddRow"
+    manga_tree.insert('', END, values=row, tags=(tag,))
 
 manga_tree.pack(expand=YES, fill=BOTH)
 
 root.mainloop()
+
